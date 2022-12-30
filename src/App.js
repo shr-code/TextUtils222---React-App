1c0b13a8156c8878d9766e7895fe25afc39768bc
Shraddha <choudharyshraddha12@gmail.com>
2022-12-30 12:05:42 +0530

    added blinking effect and alerts to app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
 // let name =<i>Shraddha</i>
 function App() {
   // return (
   // <>
   // <h1> this is Shraddha</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React with Shraddha
-    //     </a>
-    //   </header>
-    // </div>
-    // </>
-    <>
-   { /* <nav>
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React with Shraddha
+  //     </a>
+  //   </header>
+  // </div>
+  // </>
+  <>
+    { /* <nav>
       <li>Home</li>
       <li>About</li>
       <li>Contact</li>
@@ -35,11 +38,43 @@ function App() {
       <h1>Hello {name}</h1>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione corporis, voluptate incidunt cupiditate omnis facere error fugit in quos mollitia voluptatum fugiat non ducimus, quisquam beatae atque! Beatae soluta ea molestiae quo?
     </div> */}
-    </>
+  </>
   // );
-  return(
- <>
- {/* <nav className="navbar navbar-expand-lg bg-light">
+  const [mode, setMode] = useState('light')
+  const toggleMode = () => {
+    if (mode === 'light') 
+    { 
+      setMode('dark') 
+      document.body.style.backgroundColor = '#042743'
+      showAlert("Dark mode has been enabled","success")
+      document.title="TextUtils222 - Dark Mode"
+      // setInterval(() => {
+      //   document.title="TextUtils222 is Amazing"
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title="Install now!"
+      // }, 1500); //for blinking effect in the document title
+    }
+    else { setMode('light') 
+    document.body.style.backgroundColor = 'white'
+    showAlert("Light mode has been enabled","success")
+    document.title="TextUtils222 - Light Mode"
+
+  }
+  }
+  const [alert, setAlert] = useState(null);
+  const showAlert =(message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },2000);
+  }
+  return (
+    <>
+      {/* <nav className="navbar navbar-expand-lg bg-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">TextUtils</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,10 +96,11 @@ function App() {
     </div>
   </div>
 </nav> */}
-<Navbar title="TextUtils222" aboutText ="About Text-Utils"/> 
-<div className="container my-3"><TextForm heading="Enter the text to analyze below"/></div>
-{/* <About/> */}
- </>
+      <Navbar title="TextUtils222" aboutText="About Text-Utils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert}/>
+      <div className="container my-3"><TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/></div>
+      {/* <About/> */}
+    </>
   );
 }
 // if you're using jsx in react you can return only one element here it is (div)app or use jsx fragment <>....</>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,59 +1,67 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
-export default function
-    TextForm(props) {
-        const [text,setText]=useState('Heya!'); //check 7 file
-        // text="comming? or not?"//can't direct set the state you have to use the function setText for the updation/modification 
-        // setText("NEeww TEXT");//correct way to change the state
-        const handleUpClick =()=>{
-            // console.log("Uppercase was clicked")
-            let newText = text.toUpperCase();
-            setText(newText)
-        }
-        const handleLoClick =()=>{
-            let newText = text.toLowerCase();
-            setText(newText)
-        }
-        const handleOnChange =(event)=>{
-            // console.log("OnChange")
-            setText(event.target.value);// whatever the targeted tag value is, set it to that and let the user able to write text
-        }
-        const handleCopyClick =()=>{
-            var text= document.getElementById("myBox");
-            text.select();
-            navigator.clipboard.writeText(text.value)
-        }
-        const handleExtraSpaces=()=>{
-            let newText=text.split(/[ ]+/);
-            setText(newText.join(" "))
-        }
+export default function TextForm(props) {
+    const [text, setText] = useState('Heya!'); //check 7 file
+    // text="comming? or not?"//can't direct set the state you have to use the function setText for the updation/modification 
+    // setText("NEeww TEXT");//correct way to change the state
+    const handleUpClick = () => {
+        // console.log("Uppercase was clicked")
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to upper case","success")
+
+    }
+    const handleLoClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+        props.showAlert("Converted to lower case","success")
+
+    }
+    const handleOnChange = (event) => {
+        // console.log("OnChange")
+        setText(event.target.value);
+        // whatever the targeted tag value is, set it to that and let the user able to write text
+    }
+    const handleCopyClick = () => {
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value)
+        props.showAlert("Text copied","success")
+
+    }
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+        props.showAlert("Removed extra spaces","success")
+
+    }
     return (
         <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+            <div className='container' style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : 'white', color: props.mode === 'dark' ? 'white' : '#042743' }}></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+                    Convert to Uppercase
+                </button>
+                <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+                    Convert to Lowercase
+                </button>
+                <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
+                    Copy text
+                </button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
+                    Remove extra spaces
+                </button>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>
-               Convert to Uppercase 
-            </button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>
-               Convert to Lowercase 
-            </button>
-            <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
-               Copy text 
-            </button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>
-               Remove extra spaces
-            </button>
-        </div>
-        <div className='container'>
-            <h3>Your text summary</h3>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").length} Minutes read</p>
-            <h3>Preview</h3>
-            <p>{text}</p>
-        </div>
+            <div className='container my-3' style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+                <h3>Your text summary</h3>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <h3>Preview</h3>
+                <p>{text.length > 0 ? text : "Enter something in the textbox to preview it"}</p>
+            </div>
         </>
     )
 }
